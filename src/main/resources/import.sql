d735a6de07d9e3755a2da36d7ec96cad41c8da2c
Luiz Barros <lck.barros@gmail.com>
2025-11-23 22:55:30 -0300

    feat: implement user, role, db seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,13 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Ana','ana@gmail.com', '$2a$10$05MSuTokUE9blYeQpz4WOeoGtXgJNA8MZAzBYGyvM/PjWT7V73Pzi');
+INSERT INTO tb_user (name, email, password) VALUES ('Bob','bob@gmail.com', '$2a$10$uucgV3XWv9Nz/9ZQg/OCruXaTudP3hsf.H6FI4zyeNnsW0jV5yBk2');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
 INSERT INTO tb_department(name) VALUES ('Sales');
 INSERT INTO tb_department(name) VALUES ('Management');
 INSERT INTO tb_department(name) VALUES ('Training');
